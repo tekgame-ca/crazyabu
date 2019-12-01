1c3cde45c546e22c5fcb6503c262d931f5dcadbc
Stephanus Parveen <stparveen.14116128@student.itera.ac.id>
2019-12-01 21:17:22 +0700

    up

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+	public float speed;
+	Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+    	rb=GetComponent<Rigidbody2D> ();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate(){
+    	Jump();
+    	float move = Input.GetAxis("Horizontal");
+    	rb.velocity = new Vector2 (speed * move , rb.velocity.y);
+    }
+
+    void Jump(){
+    	if(Input.GetButtonDown("Jump")){
+    		gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 5f),ForceMode2D.Impulse);
+    	}
+    }
+}	
+
